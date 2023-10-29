73a9b64f08d37b165070dfd91703bf297dd45f95
Landin Kasti <lkastiman@gmail.com>
2023-10-29 16:17:18 -0500

    Added movement functionality with drag included

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,38 +4,72 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     private float verticalInput;
     private float horizontalInput;
     public float speed;
     private Rigidbody playerRB;
-    private AudioSource playerAudio;
+    Vector3 moveDirection;
+    public Transform orientation;
     public float gravMod = 1.2f;
-    private Vector3 latestCP;
-    public float cameraRotSpeed;
+
+
+    public float groundDrag;
+    [Header("Ground Status")]
+    public float playerHeight;
+    public LayerMask theGround;
+    bool grounded;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        playerRB.freezeRotation = true;
         Physics.gravity *= gravMod;
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement();
-        rotPlayer();
+        // is player on ground?
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, theGround);
+
+        movementInput();
+        speedControl();
+
+        // applying drag
+        if(grounded)
+            playerRB.drag = groundDrag;
+        else
+            playerRB.drag = 0;
+
     }
 
-    private void movement(){
+    void FixedUpdate()
+    {
+        playerMovement();    
+    }
+
+    private void movementInput(){
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
 
-        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-    }
-
-    private void rotPlayer(){
         
     }
+
+    private void speedControl(){
+        Vector3 flatVel = new Vector3(playerRB.velocity.x, 0f, playerRB.velocity.z);
+
+        if(flatVel.magnitude > speed){
+            Vector3 velLimit = flatVel.normalized * speed;
+            playerRB.velocity = new Vector3(velLimit.x, playerRB.velocity.y, velLimit.z);
+        }
+    }
+
+    private void playerMovement(){
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        playerRB.AddForce(moveDirection.normalized * speed * 10f, ForceMode.Force);
+    }
+
 }
